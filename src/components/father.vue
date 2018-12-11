244ab9499e43ce26fcf556bf35eca12b1daea4ff
ct <978744151@qq.com>
2018-12-11 14:35:35 +0800

    父子组件传参

---
+++ src/components/father.vue
@@ -0,0 +1,47 @@
+<template>
+<div>
+    <button @click="clickParent">{{yuandou}}</button>
+    <son ref="mychild" :shuxing = "yuandou" @event="getuser"></son>
+  </div>
+</template>
+
+<script>
+ import son from './son';
+export default {
+    data() {
+      return {
+            // radio:'',
+            c_zfb: true,
+            c_wx: false,
+            money: 'true',
+            orderlist: [],
+            rmb: '10',
+            yuandou: '1000',
+            access_code: '',
+            userid: '',
+            appVersion:'',
+            isDisable: false,
+            yuandouyue:'',
+            channelList:'',
+            zfbpay:'',
+            wxpay:''
+        }
+    },
+    name: "parent",
+    components: {
+      son:son
+    },
+    methods: {
+      clickParent(e) {
+        this.$refs.mychild.parentHandleclick(e);
+      },
+      getuser(e){
+        console.log(e)
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/son.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>son</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            obj:{
+                num:'12',
+                sas:'123'
+            }
+        }
+    },
+    name: "son",
+    props: ['shuxing'],
+    methods: {
+      parentHandleclick(e) {
+        console.log(this.shuxing)
+        this.$emit('event',this.obj)
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
